7bbe8b62aff5c25d87971d3312b4b4ba05c3de86
Felipe <felipejofre2001@gmail.com>
2024-02-20 18:41:26 -0600

    Button updates now

--- src/buttons.js
+++ src/buttons.js
@@ -3,22 +3,35 @@ import Form from './forms'
 
 function CreateProject(props) {
     const [inputValue, setInputValue] = useState('');
+    const [projectName, setProjectName] = useState('');
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleCreateProject = () => {
+        setProjectName(inputValue);
+    };
 
     return (
         <div>
             <center>
-            <Form/>
-            <button onClick={() => setInputValue(FormData.arguments.inputValue)}>
-            Create Project
-            </button>
-            <p>Project Name: {inputValue}</p>
+                <form>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        placeholder="Enter project name"
+                    />
+                    <button type="button" onClick={handleCreateProject}>
+                        Create Project
+                    </button>
+                </form>
+                <p>Project Name: {projectName}</p>
             </center>
-            
-            
         </div>
-  );
-
-
+    );
 }
+
 
 export default CreateProject;
