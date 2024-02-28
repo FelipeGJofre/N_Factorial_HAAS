c3ca1ae2abfe520992661be52e550dbea16079a4
e-turcotte <eturcotte@utexas.edu>
2024-02-28 02:12:26 -0600

    Implement sign up and login functionality with database

--- backend/backend_main.py
+++ backend/backend_main.py
@@ -5,8 +5,8 @@ import json
 
 app = Flask(__name__, template_folder='./')
 
-@app.route("/login",  methods=["GET", "POST"])
-def login():
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
     data = request.json
     # Process the received data
     file = open("database.txt", "a")
@@ -14,7 +14,20 @@ def login():
     file.write("\n")
     file.close()
     print(data)
-    return jsonify({'message': 'Received login data successfully'})
+    return jsonify({'message': 'Received sign up data successfully'})
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    querydata = request.json
+    # Process the received data
+    file = open("database.txt", "r")
+    for line in file.readlines():
+        dbdata = json.loads(line)
+        if querydata['email'] == dbdata['email'] and querydata['password'] == dbdata['password']:
+            rtnmsg = "Hello, " + dbdata['user'] + "!"
+    file.close()
+    print(rtnmsg)
+    return jsonify({'message': 'Found user data successfully'})
 
 
 # Define a route for the "/about" URL with GET met
--- src/LoginSignUp.js
+++ src/LoginSignUp.js
@@ -6,20 +6,20 @@ import password_icon from './Assets/password.png'
 
 const LoginSignUp = () => {
     const [action, setAction] = useState("Sign Up")
-    const [login_info, setData] = useState({
+    const [user_info, userData] = useState({
         user: "",
         email: "",
         password: ""
     })
 
-    const handleLogin = () => {
-        console.log(login_info)
-        fetch('/login', {
+    const handleSignUp = () => {
+        console.log(user_info)
+        fetch('/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(login_info),
+            body: JSON.stringify(user_info),
         })
         .then(response => {
             if (!response.ok) {
@@ -35,20 +35,50 @@ const LoginSignUp = () => {
             // Handle error
             console.error('There was a problem with your fetch operation:', error);
         })
-        setData({
+        userData({
             user: "",
             email: "",
             password: ""
         })
     }
+
+    const handleLogin = () => {
+        console.log(user_info)
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user_info),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle response data from Flask if needed
+            console.log(data);
+        })
+        .catch(error => {
+            // Handle error
+            console.error('There was a problem with your fetch operation:', error);
+        })
+        userData({
+            email: "",
+            password: ""
+        })
+    }
+
     const handleInputChange = (event) => {
         const { name, value } = event.target;
         console.log(name)
-        setData(prevState => ({ 
+        userData(prevState => ({ 
             ...prevState,
             [name]: value }));
     };
-  return (
+    return (
     <div className='container'>
         <div className='header'>
             <div className='text'>{action}</div>
@@ -56,27 +86,22 @@ const LoginSignUp = () => {
         <div className='input-div'>
             {action=== "Login"? null : <div className="input">
                 <img src={user_icon} alt="" />
-                <input type="text" placeholder="Username" name="user" id="" value={login_info.user} onChange={handleInputChange}/>
+                <input type="text" placeholder="Username" name="user" id="" value={user_info.user} onChange={handleInputChange}/>
             </div>}
             <div className="input">
                 <img src={email_icon} alt="" />
-                <input type="email" placeholder="Email" name="email" id="" value={login_info.email} onChange={handleInputChange}/>
+                <input type="email" placeholder="Email" name="email" id="" value={user_info.email} onChange={handleInputChange}/>
             </div>
             <div className="input">
                 <img src={password_icon} alt="" />
-                <input type="password" placeholder="Password" name="password" id="" value={login_info.password} onChange={handleInputChange}/>
+                <input type="password" placeholder="Password" name="password" id="" value={user_info.password} onChange={handleInputChange}/>
             </div>
         </div>
+        {action==="Sign Up"? null :<div className="forgot-password">Don't have an account? <span onClick={()=>{setAction("Sign Up")}}>Click here!</span></div>}
         {action==="Sign Up"? null :<div className="forgot-password">Forgot Password? <span>Click here!</span></div>}
-        <div className="submitButton">
-            <center>
-                <button type="button" onClick={handleLogin}> Log In </button>
-            </center>
-        </div>
-        
+        {action==="Login"? null :<div className="forgot-password">Already have an account? <span onClick={()=>{setAction("Login")}}>Click here!</span></div>}
         <div className="submit-container">
-            <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Submit</div>
-            <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+            <div className="submit" onClick={action==="Sign Up"? handleSignUp : handleLogin}>Submit</div>
         </div>
     </div>
   )
