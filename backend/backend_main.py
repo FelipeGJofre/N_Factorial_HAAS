1c00db9204de2c66322dc4468ad5f5f8064e6b14
e-turcotte <eturcotte@utexas.edu>
2024-03-08 02:10:09 -0600

    Add hashing to communications with faux database

--- backend/backend_main.py
+++ backend/backend_main.py
@@ -10,8 +10,7 @@ def signup():
     data = request.json
     # Process the received data
     file = open("database.txt", "a")
-    json.dump(data, file)
-    file.write("\n")
+    file.write(encrypt(json.dumps(data), 3, 1) + "\n")
     file.close()
     print(data)
     return jsonify({'message': 'Received sign up data successfully'})
@@ -22,12 +21,20 @@ def login():
     # Process the received data
     file = open("database.txt", "r")
     for line in file.readlines():
-        dbdata = json.loads(line)
+        dbdata = json.loads(decrypt(line.strip(), 3, 1))
         if querydata['email'] == dbdata['email'] and querydata['password'] == dbdata['password']:
-            rtnmsg = "Hello, " + dbdata['user'] + "!"
+            rtnmsg = "Hello, " + dbdata['user']
     file.close()
     print(rtnmsg)
     return jsonify({'message': 'Found user data successfully'})
+
+def encrypt(text, n, d):
+    return "".join([chr(((ord(c) + (n * d)) % 128)) for c in text[::-1]])
+
+
+
+def decrypt(text, n, d):
+    return "".join([chr(((ord(c) + (n * d * -1)) % 128)) for c in text[::-1]])
 
 
 # Define a route for the "/about" URL with GET met
