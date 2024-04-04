8874a1a819f537744a96f562601658b73339e4a6
Tarun Rajanala <tarunrajanala@utexas.edu>
2024-04-03 22:02:09 -0500

    Login and CheckIn/Out Framework

---
+++ src/LoginwProjects.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User'); //May need to implement or change this
+const Project = require('./models/Project'); 
+
+const app = express();
+
+app.use(express.json());
+
+//Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.post('/login', async (req, res) => {
+    const { userID, password } = req.body;
+
+    //Find the user in the database
+    const user = await User.findOne({ userID, password });
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid userID or password' });
+    }
+
+    // Find the projects that the user has joined
+    const projects = await Project.find({ _id: { $in: user.projects } });
+
+    // Return the user's projects
+    return res.json({ projects });
+});
